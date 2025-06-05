20f2d4f171213e0c9ad90d7f4eb0a38f646c1a32
marufa <gmalfacorrea@gmail.com>
2025-06-04 21:13:23 -0300

    Correção Exercicio4.java

--- src/main/java/org/example/Exercicio4/Exercicio4.java
+++ src/main/java/org/example/Exercicio4/Exercicio4.java
@@ -1,12 +1,11 @@
 package org.example.Exercicio4;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
+
+import static org.example.HttpResponseUtil.getConnection;
+import static org.example.HttpResponseUtil.respostaRequisicao;
 
 public class Exercicio4 {
     public static void main(String[] args){
@@ -18,34 +17,11 @@ public class Exercicio4 {
     }
 
     private static void paramFic() throws URISyntaxException, IOException {
-        BufferedReader reader;
-        StringBuilder response = new StringBuilder();
-        URL url = new URI("https://apichallenges.eviltester.com/sim/entities?categoria=teste&limite=5").toURL();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        HttpURLConnection connection = getConnection("https://apichallenges.eviltester.com/sim/entities?categoria=teste&limite=5", "GET");
+
         try{
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept", "applicaton/json");
-            int responseCode = connection.getResponseCode();
-
-            System.out.println("Código resposta: " + responseCode);
-
-            if(responseCode >= HttpURLConnection.HTTP_OK && responseCode < HttpURLConnection.HTTP_MULT_CHOICE){
-                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                while((inputLine = reader.readLine()) != null) {
-                    response.append(inputLine).append("\n");
-                }
-            }else {
-                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-                String inputLine;
-                while((inputLine = reader.readLine()) != null) {
-                    response.append(inputLine).append("\n");
-                }
-            }
-
-            System.out.println("Resposta do Request: " + response);
-            reader.close();
-
+            System.out.println(respostaRequisicao(connection));
+            System.out.println(connection.getResponseCode());
         }catch (Exception e){
             System.out.println(e.getMessage());
         }finally{
